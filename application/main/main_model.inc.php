710141afafbee673196475a4c363094795b50295
Serg Ctiki <ctikitsg@gmail.com>
2016-01-20 19:08:11 +0200

    Добавлен метод getLastWorksModel

--- application/main/main_model.inc.php
+++ application/main/main_model.inc.php
@@ -1,7 +1,22 @@
 <?php
 class Main_Model extends Model{
 
-    public function getContent(){
-        return array('ContentModel' => 'MainPageModel');
+    final public function getLastWorksModel(){
+        $sth = Database::$DB->prepare("SELECT gallery.*, services.eng_title\n"
+                                    ." FROM `gallery`\n"
+                                    ." RIGHT JOIN services\n"
+                                    ." ON gallery.id_category=services.id\n"
+                                    ." ORDER BY `date` DESC\n"
+                                    ." LIMIT 0,6");
+        $sth->execute();
+        if ($sth){
+            while($row = $sth->fetch(PDO::FETCH_ASSOC)){
+                $result[] = $row;
+            }
+        }
+        else{
+            $result = 'Ошибка';
+        }
+        return $result;
     }
 }
